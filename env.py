bebd7865dd9d9c44b1a0bd7fc4d8bbcdc4bd6d3d
Sthope <6695214+Sthopeless@users.noreply.github.com>
2021-07-09 15:39:45 +0200

    Create env.py

---
+++ env.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#### iot.tuya.com ##################
+ACCESS_ID = ""
+ACCESS_KEY = ""
+ENDPOINT = "https://openapi.tuyaeu.com"
+###################################
+#### Mobile APP ####################
+USERNAME = ""
+PASSWORD = ""
+COUNTRY_CODE = "351"
+SCHEMA = "smartlife"
+###################################
+ASSET_ID = ""
+DEVICE_ID = ""
